03d1193eeed4bf3c33cab1f65df944b7df562b95
rado1971 <rado1971@abv.bg>
2026-08-07 08:27:47 +0300

    Update moneti_bnb.py

--- moneti_bnb.py
+++ moneti_bnb.py
@@ -6,5 +6,8 @@ print("проверка на юбилейни монети")
 Python
 today=datetime.date.today()
 print("Днес е:",today)
-
+Python
+with open("config.json","r")as file:config=json.load(file)
+print("Имейл за известия:",config["email]
+      
       
